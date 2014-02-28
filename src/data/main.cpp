1e2736d69a5a3cd6ee2d030e61fb0c9cd439174c
Robert Leahy <rleahy@rleahy.ca>
2014-02-28 12:08:58 -0800

    UCD Parser Command Line Arguments
    The UCD parser now accepts a path to the Unicode Character Database (unzipped) as its first command line argument, a path to the output file as its second.

--- src/data/main.cpp
+++ src/data/main.cpp
@@ -1,15 +1,33 @@
 #include "parser.hpp"
+#include <cstdlib>
+#include <iostream>
 
 
-int main (void) {
+int main (int argc, char ** argv) {
 
+	if (argc<3) {
+	
+		std::cout << "Expected 2 command line arguments, " << (argc-1) << " provided" << std::endl;
+		
+		return EXIT_FAILURE;
+	
+	}
+	
 	Parser parser(
-		"C:\\Users\\rleahy\\Documents\\C++\\Unicode\\UCD\\",
-		"C:\\Users\\rleahy\\Documents\\C++\\Unicode\\bin\\test.cpp"
+		argv[1],
+		argv[2]
 	);
+	
+	std::cout << "Reading Unicode character database..." << std::endl;
 	
 	parser.Get();
 	
+	std::cout << "Done!" << std::endl;
+	
+	std::cout << "Writing C++ source file..." << std::endl;
+	
 	parser.Output();
+	
+	std::cout << "Done!" << std::endl;
 
 }
