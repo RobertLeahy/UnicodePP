58982e31645f5b574e6fa9ddec28afb9969d284e
Robert Leahy <rleahy@rleahy.ca>
2014-03-01 08:57:01 -0800

    UCD Build Rule
    Added a rule to the build system to build the data.cpp file by using the UCD parser when it is not present/outdated.

--- build/windows/data.mk
+++ build/windows/data.mk
@@ -14,3 +14,9 @@ obj/parsing/parser.o \
 obj/parsing/tointeger.o | \
 bin/unicodedata.dll
 	$(GPP) -o $@ $^ bin/unicodedata.dll
+	
+	
+src/data/data.cpp: \
+bin/data.exe
+	$(call MKDIR,$@)
+	bin/data.exe ./UCD ./src/data/data.cpp
