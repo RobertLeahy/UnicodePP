2cff16342008a454bf8ae0d547c0258e43cf0993
Robert Leahy <rleahy@rleahy.ca>
2014-03-03 21:22:15 -0800

    String Documentation
    Documented the String class.

--- include/unicode/string.hpp
+++ include/unicode/string.hpp
@@ -19,6 +19,9 @@
 namespace Unicode {
 
 
+	/**
+	 *	Encapsulates a C-style string.
+	 */
 	class CString {
 	
 	
@@ -29,8 +32,13 @@ namespace Unicode {
 			
 			
 		public:
-			
-			
+		
+		
+			/**
+			 *	\cond
+			 */
+			 
+			 
 			CString (std::vector<unsigned char> vec) : vec(std::move(vec)) {
 			
 				vec.push_back(0);
@@ -38,6 +46,17 @@ namespace Unicode {
 			}
 			
 			
+			/**
+			 *	\endcond
+			 */
+			
+			
+			/**
+			 *	Retrieves a pointer to a null-terminated string.
+			 *
+			 *	\return
+			 *		A pointer to a null-terminated string.
+			 */
 			operator const char * () const noexcept {
 			
 				return reinterpret_cast<const char *>(vec.data());
@@ -45,6 +64,15 @@ namespace Unicode {
 			}
 			
 			
+			/**
+			 *	Determines the size of the string in bytes.
+			 *
+			 *	Unlike calling std::strlen on an actual C-style
+			 *	string, this operation is O(1).
+			 *
+			 *	\return
+			 *		The size of the string in bytes.
+			 */
 			std::size_t Size () const noexcept {
 			
 				return vec.size()-1;
@@ -55,6 +83,9 @@ namespace Unicode {
 	};
 
 
+	/**
+	 *	Encapsulates a Unicode string.
+	 */
 	class String {
 	
 	
@@ -84,9 +115,38 @@ namespace Unicode {
 		public:
 		
 		
+			/**
+			 *	Creates a new, empty string.
+			 */
 			String () noexcept : locale(nullptr) {	}
+			/**
+			 *	Creates a new, empty string which will use a
+			 *	certain locale for its operations.
+			 *
+			 *	\param [in] locale
+			 *		The locale to use.
+			 */
 			explicit String (const Locale & locale) noexcept : locale(&locale) {	}
+			/**
+			 *	Creates a new string containing a sequence of
+			 *	Unicode code points.
+			 *
+			 *	\param [in] cps
+			 *		A vector containing the sequence of code
+			 *		points with which to initialize the string.
+			 */
 			String (std::vector<CodePoint> cps) noexcept : cps(std::move(cps)), locale(nullptr) {	}
+			/**
+			 *	Creates a new string containing a sequence of
+			 *	Unicode code points, and which will use a certain
+			 *	locale for its operations.
+			 *
+			 *	\param [in] cps
+			 *		A vector containing the sequence of code
+			 *		points with which to initialize the string.
+			 *	\param [in] locale
+			 *		The locale to use.
+			 */
 			String (std::vector<CodePoint> cps, const Locale & locale) noexcept : cps(std::move(cps)), locale(&locale) {	}
 			
 			
@@ -96,6 +156,20 @@ namespace Unicode {
 			String & operator = (String &&) = default;
 			
 			
+			/**
+			 *	Overwrites this string with a sequence of code
+			 *	points.
+			 *
+			 *	After this operation completes, the string's
+			 *	internal locale will be reset to default.
+			 *
+			 *	\param [in] cps
+			 *		A vector containing the sequence of code
+			 *		points with which to overwrite the string.
+			 *
+			 *	\return
+			 *		A reference to this string.
+			 */
 			String & operator = (std::vector<CodePoint> cps) noexcept {
 			
 				locale=nullptr;
@@ -106,31 +180,66 @@ namespace Unicode {
 			}
 			
 			
+			/**
+			 *	Creates a string from a C string.
+			 *
+			 *	\param [in] str
+			 *		A pointer to a null-terminated sequence of
+			 *		UTF-8 characters.
+			 */
 			String (const char * str);
+			/**
+			 *	Overwrites this string with a C string.
+			 *
+			 *	\param [in] str
+			 *		A pointer to a null-terminated sequence of
+			 *		UTF-8 characters.
+			 *
+			 *	\return
+			 *		A reference to this string.
+			 */
 			String & operator = (const char * str);
 			
 			
+			/**
+			 *	Retrieves an iterator to the beginning of the string.
+			 *
+			 *	\return
+			 *		An iterator to the beginning of the string.
+			 */
 			const CodePoint * begin () const noexcept {
 			
 				return Begin(cps);
 			
 			}
-			
-			
+			/**
+			 *	Retrieves an iterator to the beginning of the string.
+			 *
+			 *	\return
+			 *		An iterator to the beginning of the string.
+			 */
 			CodePoint * begin () noexcept {
 			
 				return Begin(cps);
 			
 			}
-			
-			
+			/**
+			 *	Retrieves an iterator to the end of the string.
+			 *
+			 *	\return
+			 *		An iterator to the end of the string.
+			 */
 			const CodePoint * end () const noexcept {
 			
 				return End(cps);
 			
 			}
-			
-			
+			/**
+			 *	Retrieves an iterator to the end of the string.
+			 *
+			 *	\return
+			 *		An iterator to the end of the string.
+			 */
 			CodePoint * end () noexcept {
 			
 				return End(cps);
@@ -138,13 +247,26 @@ namespace Unicode {
 			}
 			
 			
+			/**
+			 *	Retrieves the locale associated with this string.
+			 *
+			 *	\return
+			 *		The locale associated with this string.  If
+			 *		there is no locale associated with this string
+			 *		the current locale is returned.
+			 */
 			const Locale & GetLocale () const noexcept {
 			
 				return (locale==nullptr) ? Locale::Get() : *locale;
 			
 			}
-			
-			
+			/**
+			 *	Associates a locale with this string, causing the
+			 *	string to use that locale for all internal operations.
+			 *
+			 *	\param [in] locale
+			 *		The locale.
+			 */
 			void SetLocale (const Locale & locale) noexcept {
 			
 				this->locale=&locale;
@@ -152,13 +274,25 @@ namespace Unicode {
 			}
 			
 			
+			/**
+			 *	Retrieves a reference to the vector of code points
+			 *	which underlies this string.
+			 *
+			 *	\return
+			 *		A reference to a vector of code points.
+			 */
 			std::vector<CodePoint> & CodePoints () noexcept {
 			
 				return cps;
 			
 			}
-			
-			
+			/**
+			 *	Retrieves a reference to the vector of code points
+			 *	which underlies this string.
+			 *
+			 *	\return
+			 *		A reference to a vector of code points.
+			 */
 			const std::vector<CodePoint> & CodePoints () const noexcept {
 			
 				return cps;
@@ -166,46 +300,291 @@ namespace Unicode {
 			}
 			
 			
+			/**
+			 *	Converts this string to a C string.
+			 *
+			 *	\param [in] utf8
+			 *		If \em true the UTF-8 encoding will be used,
+			 *		otherwise the string will be converted to
+			 *		Latin-1.  Defaults to \em true.
+			 *
+			 *	\return
+			 *		An object which is implicitly convertible to
+			 *		const char *, but which manages the necessary
+			 *		dynamically-allocated memory using the RAII
+			 *		idiom.
+			 */
 			CString ToCString (bool utf8=true) const;
 			
 			
+			/**
+			 *	Removes all whitespace from the beginning of this
+			 *	string.
+			 *
+			 *	\return
+			 *		A copy of this string with all whitespace
+			 *		removed from the beginning.
+			 */
 			String TrimFront () const &;
+			/**
+			 *	Removes all whitespace from the beginning of this
+			 *	string.
+			 *
+			 *	\return
+			 *		A reference to this string.
+			 */
 			String & TrimFront () & noexcept;
+			/**
+			 *	Removes all whitespace from the beginning of this
+			 *	string.
+			 *
+			 *	\return
+			 *		This string.
+			 */
 			String TrimFront () && noexcept;
 			
+			
+			/**
+			 *	Removes all whitespace from the end of this
+			 *	string.
+			 *
+			 *	\return
+			 *		A copy of this string with all whitespace
+			 *		removed from the end.
+			 */
 			String TrimRear () const &;
+			/**
+			 *	Removes all whitespace from the end of this
+			 *	string.
+			 *
+			 *	\return
+			 *		A reference to this string.
+			 */
 			String & TrimRear () & noexcept;
+			/**
+			 *	Removes all whitespace from the end of this
+			 *	string.
+			 *
+			 *	\return
+			 *		This string.
+			 */
 			String TrimRear () && noexcept;
 			
+			
+			/**
+			 *	Removes all whitespace from both the beginning
+			 *	and end of this string.
+			 *
+			 *	\return
+			 *		A copy of this string with all whitespace
+			 *		removed from the beginning and end.
+			 */
 			String Trim () const &;
+			/**
+			 *	Removes all whitespace from both the beginning
+			 *	and end of this string.
+			 *
+			 *	\return
+			 *		A reference to this string.
+			 */
 			String & Trim () & noexcept;
+			/**
+			 *	Removes all whitespace from both the beginning
+			 *	and end of this string.
+			 *
+			 *	\return
+			 *		This string.
+			 */
 			String Trim () && noexcept;
 			
 			
+			/**
+			 *	Converts this string to upper case.
+			 *
+			 *	\param [in] full
+			 *		If \em true full case mappings will be
+			 *		used.  Defaults to \em true.
+			 *
+			 *	\return
+			 *		A copy of this string with each character
+			 *		converted to upper case.
+			 */
 			String ToUpper (bool full=true) const &;
+			/**
+			 *	Converts this string to upper case.
+			 *
+			 *	\param [in] full
+			 *		If \em true full case mappings will be
+			 *		used.  Defaults to \em true.
+			 *
+			 *	\return
+			 *		A reference to this string.
+			 */
 			String & ToUpper (bool full=true) &;
 			
+			
+			/**
+			 *	Converts this string to lower case.
+			 *
+			 *	\param [in] full
+			 *		If \em true full case mappings will be
+			 *		used.  Defaults to \em true.
+			 *
+			 *	\return
+			 *		A copy of this string with each character
+			 *		converted to upper case.
+			 */
 			String ToLower (bool full=true) const &;
+			/**
+			 *	Converts this string to lower case.
+			 *
+			 *	\param [in] full
+			 *		If \em true full case mappings will be
+			 *		used.  Defaults to \em true.
+			 *
+			 *	\return
+			 *		A reference to this string.
+			 */
 			String & ToLower (bool full=true) &;
 			
+			
+			/**
+			 *	Performs case folding on this string.
+			 *
+			 *	\param [in] full
+			 *		If \em true full case mappings will be
+			 *		used.  Defaults to \em true.
+			 *
+			 *	\return
+			 *		A copy of this string with each character
+			 *		case folded.
+			 */
 			String ToCaseFold (bool full=true) const &;
+			/**
+			 *	Performs case folding on this string.
+			 *
+			 *	\param [in] full
+			 *		If \em true full case mappings will be
+			 *		used.  Defaults to \em true.
+			 *
+			 *	\return
+			 *		A reference to this string.
+			 */
 			String & ToCaseFold (bool full=true) &;
 			
 			
+			/**
+			 *	Determines if this string is in Normal Form
+			 *	Canonical Decomposition.
+			 *
+			 *	Note that this method is permitted to spuriously
+			 *	return \em false, but will never spuriously
+			 *	return \em true.
+			 *
+			 *	\return
+			 *		\em true if this string is in NFD, \em false
+			 *		otherwise.
+			 */
 			bool IsNFD () const noexcept;
+			/**
+			 *	Determines if this string is in Normal Form
+			 *	Canonical Composition.
+			 *
+			 *	Note that this method is permitted to spuriously
+			 *	return \em false, but will never spuriously return
+			 *	\em true.
+			 *
+			 *	\return
+			 *		\em true if this string is in NFC, \em false
+			 *		otherwise.
+			 */
 			bool IsNFC () const noexcept;
 			
 			
+			/**
+			 *	Converts this string to Normal Form Canonical
+			 *	Decomposition.
+			 *
+			 *	\return
+			 *		A copy of this string converted to NFD.
+			 */
 			String ToNFD () const &;
+			/**
+			 *	Converts this string to Normal Form Canonical
+			 *	Decomposition.
+			 *
+			 *	\return
+			 *		A reference to this string.
+			 */
 			String & ToNFD () &;
+			/**
+			 *	Converts this string to Normal Form Canonical
+			 *	Decomposition.
+			 *
+			 *	\return
+			 *		This string.
+			 */
 			String ToNFD () &&;
 			
+			
+			/**
+			 *	Converts this string to Normal Form Canonical
+			 *	Composition.
+			 *
+			 *	\return
+			 *		A copy of this string converted to NFC.
+			 */
 			String ToNFC () const &;
+			/**
+			 *	Converts this string to Normal Form Canonical
+			 *	Composition.
+			 *
+			 *	\return
+			 *		A reference to this string.
+			 */
 			String & ToNFC () &;
+			/**
+			 *	Converts this string to Normal Form Canonical
+			 *	Composition.
+			 *
+			 *	\return
+			 *		This string.
+			 */
 			String ToNFC () &&;
 			
 			
+			/**
+			 *	Determines whether or not this string and another
+			 *	string are canonically equivalent.
+			 *
+			 *	\param [in] other
+			 *		The string with which to compare this string.
+			 *	\param [in] case_sensitive
+			 *		If \em false the case of characters shall be
+			 *		ignored when comparing this string and \em other.
+			 *		Defaults to \em true.
+			 *
+			 *	\return
+			 *		\em true if this string and \em other should be
+			 *		considered equivalent, \em false otherwise.
+			 */
 			bool Equals (const String & other, bool case_sensitive=true) const;
+			/**
+			 *	Determines whether or not this string and another
+			 *	string are canonically equivalent.
+			 *
+			 *	\param [in] str
+			 *		A C style string with which to compare this
+			 *		string.
+			 *	\param [in] case_sensitive
+			 *		If \em false the case of characters shall be
+			 *		ignored when comparing this string and \em other.
+			 *		Defaults to \em true.
+			 *
+			 *	\return
+			 *		\em true if this string and \em other should be
+			 *		considered equivalent, \em false otherwise.
+			 */
 			bool Equals (const char * str, bool case_sensitive=true) const;
 	
 	
