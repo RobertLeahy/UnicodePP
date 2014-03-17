ea9f970e6b95ac3fed652b542b3989f798fa0a78
Robert Leahy <rleahy@rleahy.ca>
2014-03-16 18:38:43 -0700

    IOStream Support Refactor
    Using the newly-added String conversion methods, was able to remove most of the code in the iostream.hpp header.

--- include/unicode/iostream.hpp
+++ include/unicode/iostream.hpp
@@ -6,75 +6,11 @@
 #pragma once
 
 
-#include <unicode/endianencoding.hpp>
 #include <unicode/string.hpp>
-#include <unicode/utf8.hpp>
-#include <unicode/utf16.hpp>
-#include <unicode/utf32.hpp>
-#include <cstddef>
 #include <iostream>
 
 
 namespace Unicode {
-
-
-	/**
-	 *	\cond
-	 */
-
-
-	namespace OutputStream {
-	
-	
-		template <typename CharT>
-		std::vector<unsigned char> Encode (const String & str) {
-		
-			constexpr std::size_t size=sizeof(CharT);
-		
-			static_assert(
-				(size==1) || (size==2) || (size==4),
-				"CharT size incompatible"
-			);
-		
-			auto s=str.ToNFC();
-			
-			switch (size) {
-			
-				case 1:
-					return UTF8{}.Encode(s);
-				case 2:{
-				
-					UTF16 encoder(EndianEncoding::Detect());
-					encoder.OutputBOM=false;
-					
-					return encoder.Encode(s);
-				
-				}
-				case 4:{
-				
-					UTF32 encoder(EndianEncoding::Detect());
-					encoder.OutputBOM=false;
-					
-					return encoder.Encode(s);
-				
-				}
-				//	This path should never be taken
-				//	in practice (static_assert above
-				//	prevents it)
-				default:
-					return std::vector<unsigned char>{};
-			
-			}
-		
-		}
-	
-	
-	}
-	
-	
-	/**
-	 *	\endcond
-	 */
 
 
 	/**
@@ -91,13 +27,7 @@ namespace Unicode {
 	template <typename CharT, typename Traits>
 	std::basic_ostream<CharT,Traits> & operator << (std::basic_ostream<CharT,Traits> & os, const String & str) {
 	
-		auto vec=OutputStream::Encode<CharT>(str);
-		os.write(
-			reinterpret_cast<const CharT *>(vec.data()),
-			vec.size()/sizeof(CharT)
-		);
-		
-		return os;
+		return os << str.ToString<CharT>();
 	
 	}
 
