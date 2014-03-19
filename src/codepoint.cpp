1d11931b40ec3bb42878d1dc920228a357ed897c
Robert Leahy <rleahy@rleahy.ca>
2014-03-18 19:28:11 -0700

    CodePoint::IsValid Overhaul
    Rewrote the CodePoint::IsValid method.  In addition to the code points it previously identified as invalid, it now detects:
    
    - The two non-characters at the end of each block
    - The contiguous range of non-characters U+FDD0..U+FDEF

--- src/codepoint.cpp
+++ src/codepoint.cpp
@@ -15,20 +15,22 @@ namespace Unicode {
 	
 	bool CodePoint::IsValid () const noexcept {
 	
-		return !(
-			//	Too large to be valid Unicode
-			(cp>Max) ||
-			//	Reserved for UTF-16 lead/trail
-			//	surrogates
-			(
-				(cp>=0xD800U) &&
-				(cp<=0xDFFFU)
-			) ||
-			//	Invalid Unicode
-			(cp==0xFFFF) ||
-			//	Invalid -- reversed BOM
-			(cp==0xFFFE)
-		);
+		//	To large to be valid Unicode
+		if (cp>Max) return false;
+		
+		//	UTF-16 lead/trail surrogate
+		if ((cp>=0xD800U) && (cp<=0xDFFFU)) return false;
+		
+		//	Arbitrary contiguous range of 32 non-character
+		//	code points
+		if ((cp>=0xFDD0U) && (cp<=0xFDEFU)) return false;
+		
+		//	Invalid Unicode
+		auto low=cp&65535U;
+		if ((low==0xFFFEU) || (low==0xFFFFU)) return false;
+		
+		//	All good
+		return true;
 	
 	}
 	
