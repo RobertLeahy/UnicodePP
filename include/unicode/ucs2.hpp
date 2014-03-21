1505ce141491435159c0007b4eb909f1599cde9c
Robert Leahy <rleahy@rleahy.ca>
2014-03-20 22:40:35 -0700

    UCS2
    Added the UCS2 class, which implements the UCS-2 encoding/decoding scheme (i.e. UTF-16 without the surrogates, restricted to BMP).
    
    Untested.

---
+++ include/unicode/ucs2.hpp
@@ -0,0 +1,59 @@
+/**
+ *	\file
+ */
+ 
+ 
+#pragma once
+
+
+#include <unicode/endianencoding.hpp>
+
+
+namespace Unicode {
+
+
+	/**
+	 *	The UCS-2 encoding.
+	 */
+	class UCS2 : public EndianEncoding {
+	
+	
+		protected:
+		
+		
+			virtual void Encoder (
+				std::vector<unsigned char> & buffer,
+				CodePoint cp
+			) const override;
+			
+			virtual std::optional<EncodingErrorType> Decoder (
+				CodePoint & cp,
+				const unsigned char * & begin,
+				const unsigned char * end,
+				std::optional<Unicode::Endianness> order
+			) const override;
+	
+	
+		public:
+		
+		
+			/**
+			 *	The type of code unit used by this
+			 *	encoding.  Unsigned integer exactly
+			 *	16 bits wide.
+			 */
+			typedef char16_t CodeUnit;
+		
+		
+			using EndianEncoding::EndianEncoding;
+			
+			
+			virtual ByteOrderMark BOM () const noexcept override;
+			virtual bool CanRepresent (CodePoint) const noexcept override;
+			virtual std::size_t Count (CodePoint) const noexcept override;
+	
+	
+	};
+
+
+}
