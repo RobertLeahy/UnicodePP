e292ed734c6fa7d5b517d8bc44328c623a7edd8e
Robert Leahy <rleahy@rleahy.ca>
2014-03-24 22:22:25 -0700

    RegexPatternElementState
    Added the RegexPatternElement State class.
    
    This class provides no members other than a virtual destructor.  Pattern elements will have the opportunity to create and store an object of this type in a smart pointer during pattern matching, thereby allowing them to save custom state in case the engine has to backtrack.

---
+++ src/regexpatternelementstate.cpp
@@ -0,0 +1,11 @@
+#include <unicode/regex.hpp>
+
+
+namespace Unicode {
+
+
+	//	Default implementation of virtual destructor
+	RegexPatternElementState::~RegexPatternElementState () noexcept {	}
+
+
+}
