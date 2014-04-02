2eeaa8477b3d568529edc023f9aa938e5f7097f9
Robert Leahy <rleahy@rleahy.ca>
2014-04-01 22:29:06 -0700

    RegexLiteral Parsing
    Rewrote/overhauled RegexLiteral parsing.
    
    The parser is now capable of handling not just literal characters, but also backslash escaped characters, as well as special backslash escapes which encapsulate specific code points (for example \u0061 and \x61 for the code point LATIN SMALL LETTER A, or \b for backspace).

--- src/regexliteral.cpp
+++ src/regexliteral.cpp
@@ -1,7 +1,11 @@
+#include <unicode/caseconverter.hpp>
 #include <unicode/codepoint.hpp>
 #include <unicode/regex.hpp>
 #include <unicode/regexcompiler.hpp>
+#include <unicode/vector.hpp>
+#include <cstddef>
 #include <limits>
+#include <optional>
 
 
 namespace Unicode {
@@ -40,6 +44,17 @@ namespace Unicode {
 					cps.push_back(cp);
 				
 				}
+				
+				
+				void Complete () {
+				
+					//	If not ignoring case, we're done
+					if (!Check(Options,RegexOptions::IgnoreCase)) return;
+					
+					//	Case fold
+					cps=CaseConverter(Locale).Fold(Begin(cps),End(cps));
+				
+				}
 		
 		
 		};
@@ -48,29 +63,179 @@ namespace Unicode {
 		class RegexLiteralParser : public RegexParser {
 		
 		
+			private:
+			
+			
+				static std::optional<CodePoint> get (const CodePoint * & begin, const CodePoint * end, std::size_t num, std::size_t base=16) {
+				
+					//	If there are no characters left, fail
+					if (begin==end) return std::nullopt;
+				
+					//	Keep track of the start point in case we
+					//	need to rewind
+					auto start=begin;
+					
+					//	Add each character
+					std::size_t i=0;
+					String s;
+					for (;(i<num) && (begin!=end);++i) s << *(begin++);
+					
+					//	If the requested number of characters are not
+					//	taken, fail
+					if (i!=num) {
+					
+						begin=start;
+						
+						return std::nullopt;
+					
+					}
+					
+					//	Attempt conversion to a code point
+					auto cp=s.To<CodePoint::Type>(base);
+					if (!cp) {
+					
+						begin=start;
+						
+						return std::nullopt;
+					
+					}
+					
+					return CodePoint(*cp);
+				
+				}
+				
+				
+				static std::size_t get_num (CodePoint cp) noexcept {
+				
+					switch (cp) {
+					
+						case 'x':
+							return 2;
+						case 'u':
+							return 4;
+						case 'U':
+							return 8;
+						default:
+							return 5;
+					
+					}
+				
+				}
+			
+			
+				static CodePoint get (const CodePoint * & begin, const CodePoint * end) {
+				
+					//	If the current code point is not a backslash, just
+					//	return the current code point
+					if (*begin!='\\') return *begin;
+					
+					//	If the current code point is a backslash, there is
+					//	custom handling -- the backslash begins an escape
+					//	sequence
+					
+					++begin;
+					
+					//	If the backslash is at the end of the string, return
+					//	the literal backslash
+					if (begin==end) return '\\';
+					
+					//	Get the next character from the stream
+					auto next=*(begin++);
+					
+					switch (next) {
+					
+						case 'a':
+							return 0x7;
+						case 'b':
+							return 0x8;
+						case 't':
+							return 0x9;
+						case 'r':
+							return 0xD;
+						case 'v':
+							return 0xB;
+						case 'f':
+							return 0xC;
+						case 'n':
+							return 0xA;
+						case 'e':
+							return 0x1B;
+						//	Various hexadecimal escapes
+						case 'x':
+						case 'u':
+						case 'U':{
+							auto start=begin;
+							auto cp=get(begin,end,get_num(next));
+							if (cp) return *cp;
+							begin=start;
+							break;
+						}
+						//	Octal escape
+						case '0':
+						case '1':
+						case '2':
+						case '3':
+						case '4':
+						case '5':
+						case '6':
+						case '7':{
+							auto start=begin;
+							--begin;
+							auto cp=get(begin,end,3,8);
+							if (cp) return *cp;
+							begin=start;
+							break;
+						}
+						default:
+							break;
+							
+					}
+					
+					//	If all else fails, take the current code point
+					//	literally
+					return next;
+				
+				}
+		
+		
 			public:
-		
-		
-				virtual bool operator () (
-					RegexCompiler::Pattern & pattern,
-					bool successive,
+			
+			
+				virtual RegexCompiler::Element operator () (
 					const CodePoint * & begin,
-					const CodePoint *,
+					const CodePoint * end,
 					RegexOptions options,
 					const Locale & locale
 				) const override {
 				
-					if (!successive) pattern.push_back(
-						RegexCompiler::Element(
-							new RegexLiteral(options,locale)
-						)
-					);
+					//	This is safe because the constructor for std::unique_ptr
+					//	is noexcept
+					auto ptr=new RegexLiteral(options,locale);
+					RegexCompiler::Element retr(ptr);
 					
-					reinterpret_cast<RegexLiteral *>(
-						pattern.back().get()
-					)->Add(*begin);
+					ptr->Add(get(begin,end));
+					
+					return retr;
+				
+				}
+				
+				
+				virtual bool operator () (
+					RegexPatternElement & element,
+					const CodePoint * & begin,
+					const CodePoint * end
+				) const override {
+				
+					reinterpret_cast<RegexLiteral &>(element).Add(get(begin,end));
 					
 					return true;
+				
+				}
+				
+				
+				virtual void Complete (RegexPatternElement & element) const override {
+				
+					reinterpret_cast<RegexLiteral &>(element).Complete();
 				
 				}
 		
