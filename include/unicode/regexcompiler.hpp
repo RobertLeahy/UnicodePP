e01fbc4f712311ba40590e0b7602b0a292fa08fc
Robert Leahy <rleahy@rleahy.ca>
2014-04-08 23:30:17 -0700

    RegexCompiler Documentation
    Added documentation to the RegexCompiler class.

--- include/unicode/regexcompiler.hpp
+++ include/unicode/regexcompiler.hpp
@@ -44,14 +44,29 @@ namespace Unicode {
 	 */
 	
 	
+	/**
+	 *	Compiles regular expression patterns.
+	 */
 	class RegexCompiler {
 	
 	
 		public:
 		
 		
+			/**
+			 *	The type of an element produced by a
+			 *	RegexCompiler.
+			 */
 			typedef std::unique_ptr<RegexPatternElement> Element;
+			/**
+			 *	The type of a compiled pattern produced
+			 *	by a RegexCompiler.
+			 */
 			typedef std::vector<Element> Pattern;
+			/**
+			 *	The iterator type RegexCompilers use to
+			 *	traverse the underlying pattern.
+			 */
 			typedef const CodePoint * Iterator;
 	
 	
@@ -89,45 +104,196 @@ namespace Unicode {
 		public:
 		
 		
+			/**
+			 *	Adds a parser to the internal collection of parsers
+			 *	RegexCompilers use to parse and compiler regular
+			 *	expression patterns.
+			 *
+			 *	\param [in] parser
+			 *		The parser.
+			 */
 			static void Add (const RegexParser & parser);
+			/**
+			 *	Adds a parser to the internal collection of parsers
+			 *	RegexCompilers use to parse and compiler regular
+			 *	expression patterns.
+			 *
+			 *	\param [in] parser
+			 *		The parser.
+			 *	\param [in] priority
+			 *		The priority with which \em parser will be
+			 *		added.  Parsers with a lower priority will be
+			 *		invoked earlier for each point in a pattern, with
+			 *		parsers with a higher priority being invoked
+			 *		later.
+			 */
 			static void Add (const RegexParser & parser, std::size_t priority);
+			/**
+			 *	Adds a parser to the internal collection of parsers
+			 *	RegexCompilers use to parse and compiler regular
+			 *	expression patterns.
+			 *
+			 *	\param [in] parser
+			 *		The parser.
+			 *	\param [in] character_class
+			 *		\em true if this parser should be invoked only
+			 *		within custom character classes, \em false if
+			 *		it should only be invoked without custom character
+			 *		classes.
+			 */
 			static void Add (const RegexParser & parser, bool character_class);
+			/**
+			 *	Adds a parser to the internal collection of parsers
+			 *	RegexCompilers use to parse and compiler regular
+			 *	expression patterns.
+			 *
+			 *	\param [in] parser
+			 *		The parser.
+			 *	\param [in] priority
+			 *		The priority with which \em parser will be
+			 *		added.  Parsers with a lower priority will be
+			 *		invoked earlier for each point in a pattern, with
+			 *		parsers with a higher priority being invoked
+			 *		later.
+			 *	\param [in] character_class
+			 *		\em true if this parser should be invoked only
+			 *		within custom character classes, \em false if
+			 *		it should only be invoked without custom character
+			 *		classes.
+			 */
 			static void Add (const RegexParser & parser, std::size_t priority, bool character_class);
 			
 			
+			/**
+			 *	Compiles a regular expression pattern.
+			 *
+			 *	\param [in] begin
+			 *		The beginning of the pattern.
+			 *	\param [in] end
+			 *		The end of the pattern.
+			 *	\param [in] options
+			 *		The options with which to compile.
+			 *	\param [in] locale
+			 *		The locale with which to compile.
+			 *
+			 *	\return
+			 *		The compiled pattern.
+			 */
 			static Pattern Compile (Iterator begin, Iterator end, RegexOptions options, const Unicode::Locale & locale);
 		
 		
+			/**
+			 *	The current location within the pattern.
+			 */
 			Iterator Current;
+			/**
+			 *	The beginning of the pattern.
+			 */
 			Iterator Begin;
+			/**
+			 *	The end of the pattern.
+			 */
 			Iterator End;
 			
 			
+			/**
+			 *	The options with which the pattern is being
+			 *	compiled.
+			 */
 			RegexOptions Options;
+			/**
+			 *	The locale with which the pattern is being
+			 *	compiled.
+			 */
 			const Unicode::Locale & Locale;
+			/**
+			 *	\em true if the currently executing parser is
+			 *	being invoked at a point in the pattern immediately
+			 *	after a point at which it already successfully
+			 *	parsed, \em false otherwise.
+			 */
 			bool Successive;
+			/**
+			 *	\em true if the compiler is compiling a custom
+			 *	character class, \em false otherwise.
+			 */
 			bool CharacterClass;
 			
 			
+			/**
+			 *	Creates a new RegexCompiler.
+			 *
+			 *	\param [in] begin
+			 *		The beginning of the pattern.
+			 *	\param [in] end
+			 *		The end of the pattern.
+			 *	\param [in] options
+			 *		The options with which to compile.
+			 *	\param [in] locale
+			 *		The locale with which to compile.
+			 */
 			RegexCompiler (Iterator begin, Iterator end, RegexOptions options, const Unicode::Locale & locale) noexcept;
+			/**
+			 *	Creates a new RegexCompiler.
+			 *
+			 *	\param [in] curr
+			 *		The location within the pattern at which
+			 *		compilation shall begin.
+			 *	\param [in] begin
+			 *		The beginning of the pattern.
+			 *	\param [in] end
+			 *		The end of the pattern.
+			 *	\param [in] options
+			 *		The options with which to compile.
+			 *	\param [in] locale
+			 *		The locale with which to compile.
+			 */
 			RegexCompiler (Iterator curr, Iterator begin, Iterator end, RegexOptions options, const Unicode::Locale & locale) noexcept;
 			
 			
+			/**
+			 *	Checks to see if the end of the pattern has been
+			 *	reached.
+			 *
+			 *	\return
+			 *		\em false if the end of the pattern has been
+			 *		reached, \em true otherwise.
+			 */
 			operator bool () const noexcept {
 			
 				return Current!=End;
 			
 			}
+			/**
+			 *	Obtains the current code point from the pattern.
+			 *
+			 *	\return
+			 *		A reference to a code point.
+			 */
 			const CodePoint & operator * () const noexcept {
 			
 				return *Current;
 			
 			}
+			/**
+			 *	Obtains a pointer to the current code point from
+			 *	the pattern.
+			 *
+			 *	\return
+			 *		A pointer to a code point.
+			 */
 			const CodePoint * operator -> () const noexcept {
 			
 				return Current;
 			
 			}
+			/**
+			 *	Advances the compiler to the next code point within
+			 *	the pattern.
+			 *
+			 *	\return
+			 *		A reference to this object.
+			 */
 			RegexCompiler & operator ++ () noexcept {
 			
 				++Current;
@@ -135,6 +301,13 @@ namespace Unicode {
 				return *this;
 			
 			}
+			/**
+			 *	Backs the compiler up to the last code point within
+			 *	the pattern.
+			 *
+			 *	\return
+			 *		A reference to this object.
+			 */
 			RegexCompiler & operator -- () noexcept {
 			
 				--Current;
@@ -144,6 +317,17 @@ namespace Unicode {
 			}
 			
 			
+			/**
+			 *	Checks to see if a certain option or options are
+			 *	set.
+			 *
+			 *	\param [in] option
+			 *		The option or options to check.
+			 *
+			 *	\return
+			 *		\em true if all options specified by \em option
+			 *		are set, \em false otherwise.
+			 */
 			bool Check (RegexOptions option) const noexcept {
 			
 				return Unicode::Check(Options,option);
@@ -151,10 +335,35 @@ namespace Unicode {
 			}
 			
 			
+			/**
+			 *	Retrieves and clears the compiled pattern.
+			 *
+			 *	\return
+			 *		The compiled pattern.
+			 */
 			Pattern Get ();
+			/**
+			 *	Clears and sets the compiled pattern.
+			 */
 			void Set (Pattern pattern);
 			
 			
+			/**
+			 *	Retrieves the last compiled RegexPatternElement
+			 *	cast to a certain type.
+			 *
+			 *	If there are no compiled RegexPatternElements, or
+			 *	the last RegexPatternElement is not of type \em T,
+			 *	the behaviour is undefined.
+			 *
+			 *	\tparam T
+			 *		The type to which the last RegexPatternElement
+			 *		shall be cast.
+			 *
+			 *	\return
+			 *		A reference to the last RegexPatternElement cast
+			 *		to type \em T.
+			 */
 			template <typename T>
 			T & Back () noexcept {
 			
@@ -163,6 +372,29 @@ namespace Unicode {
 			}
 			
 			
+			/**
+			 *	Adds a RegexPatternElement of a certain type to
+			 *	the back of the pattern.
+			 *
+			 *	Options and Locale will always be passed,
+			 *	respectively, as the last two arguments to the
+			 *	constructor of \em T and do not have to be
+			 *	provided.
+			 *
+			 *	\tparam T
+			 *		The type of RegexPatternElement to add.
+			 *	\tparam Args
+			 *		The types of the arguments to forward through
+			 *		to the constructor of \em T.
+			 *
+			 *	\param [in] args
+			 *		The arguments of types \em Args which shall
+			 *		be forwarded through to the constructor of
+			 *		\em T.
+			 *
+			 *	\return
+			 *		A reference to the newly-created RegexPatternElement.
+			 */
 			template <typename T, typename... Args>
 			T & Add (Args &&... args) {
 			
@@ -185,13 +417,36 @@ namespace Unicode {
 			}
 			
 			
+			/**
+			 *	Throws a RegexError with a certain error message.
+			 *
+			 *	\param [in] what
+			 *		The error message.
+			 */
 			[[noreturn]]
 			void Raise (const char * what) const;
 			
 			
+			/**
+			 *	Checks to see if compilation is finished at this
+			 *	point in the string.
+			 *
+			 *	Default implementation returns \em true only when
+			 *	operator bool returns \em false.
+			 *
+			 *	If \em true is not returned when operator bool
+			 *	returns \em false, the behaviour is undefined.
+			 *
+			 *	\return
+			 *		\em true if compilation is finished, \em false
+			 *		otherwise.
+			 */
 			virtual bool Done ();
 			
 			
+			/**
+			 *	Compiles the regular expression pattern.
+			 */
 			void operator () ();
 	
 	
