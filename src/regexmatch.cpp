b4b677508e83b39ebe68a3b7edc5e59935e142ab
Robert Leahy <rleahy@rleahy.ca>
2014-04-11 01:53:02 -0700

    RegexMatch Cleanup
    Cleaned up the implementation of the RegexMatch::operator [] overloads.

--- src/regexmatch.cpp
+++ src/regexmatch.cpp
@@ -36,40 +36,23 @@ namespace Unicode {
 	}
 	
 	
-	template <typename T>
-	RegexMatch::Type & emplace (T && key, std::unordered_map<typename std::decay<T>::type,RegexMatch::Type> & map) {
-	
-		return map.emplace(std::forward<T>(key),RegexMatch::Type{}).first->second;
-	
-	}
-	
-	
-	template <typename T>
-	RegexMatch::Type & retrieve (const T & key, std::unordered_map<T,RegexMatch::Type> & map) {
-	
-		auto iter=map.find(key);
-		return (iter==map.end()) ? emplace(key,map) : iter->second;
-	
-	}
-	
-	
 	RegexMatch::Type & RegexMatch::operator [] (const String & key) {
 	
-		return retrieve(key,named);
+		return named[key];
 	
 	}
 	
 	
 	RegexMatch::Type & RegexMatch::operator [] (String && key) {
 	
-		return emplace(std::move(key),named);
+		return named[std::move(key)];
 	
 	}
 	
 	
 	RegexMatch::Type & RegexMatch::operator [] (std::size_t key) {
 	
-		return retrieve(key,numbered);
+		return numbered[key];
 	
 	}
 
