203d5a55387bb6a112fc67bdae4b147ef76a2e95
Robert Leahy <rleahy@rleahy.ca>
2014-04-16 19:06:01 -0700

    E-Mail Regex Miscellaneous Unit Test
    With the addition of basic grouping constructs, the regular expression engine supports all the constructs necessary to compile and run the e-mail validation regex from here: http://www.regular-expressions.info/email.html
    
    Added a miscellaneous unit test which uses this regex to validate various e-mail addresses as a means of testing the various parts of the engine in action together.

--- build/windows/test.mk
+++ build/windows/test.mk
@@ -10,7 +10,8 @@ bin/tests.exe
 bin/regex_tests.exe: \
 obj/test/catch.o \
 obj/test/main.o \
-obj/test/regex/anchor.o | \
+obj/test/regex/anchor.o \
+obj/test/regex/misc.o | \
 bin/unicode.dll
 	$(GPP) -o $@ $^ bin/unicode.dll
 	bin/regex_tests.exe
---
+++ src/test/regex/misc.cpp
@@ -0,0 +1,138 @@
+#include <unicode/regex.hpp>
+#include "../catch.hpp"
+#include <catch.hpp>
+
+
+using namespace Unicode;
+
+
+SCENARIO("RFC 5322 e-mail regex successfully validates e-mails","[misc]") {
+
+	GIVEN("RFC 5322 e-mail regex which allows leading and trailing white space") {
+	
+		Regex r(
+			R"~~(^\s*
+			  (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*
+			  |  "(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]
+				  |  \\[\x01-\x09\x0b\x0c\x0e-\x7f])*")
+			@ (?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?
+			  |  \[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}
+				   (?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:
+					  (?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]
+					  |  \\[\x01-\x09\x0b\x0c\x0e-\x7f])+)
+				 \])
+			\s*$)~~",
+			RegexOptions::IgnorePatternWhiteSpace|RegexOptions::IgnoreCase
+		);
+		
+		WHEN("\"rleahy@rleahy.ca\" is tested") {
+		
+			auto match=r.Match("rleahy@rleahy.ca");
+		
+			THEN("It is found to be a valid e-mail") {
+			
+				CHECK(match);
+			
+			}
+		
+		}
+		
+		WHEN("\" rleahy@rleahy.ca \" is tested") {
+		
+			auto match=r.Match(" rleahy@rleahy.ca ");
+		
+			THEN("It is found to be a valid e-mail") {
+			
+				CHECK(match);
+			
+			}
+		
+		}
+		
+		WHEN("\"RLEAHY@RLEAHY.CA\" is tested") {
+		
+			auto match=r.Match("RLEAHY@RLEAHY.CA");
+			
+			THEN("It is found to be a valid e-mail") {
+			
+				CHECK(match);
+			
+			}
+		
+		}
+		
+		WHEN("\"rleahy.ca\" is tested") {
+		
+			auto match=r.Match("rleahy.ca");
+			
+			THEN("It is not found to be a valid e-mail") {
+			
+				CHECK(!match);
+			
+			}
+		
+		}
+		
+		WHEN("\"@rleahy.ca\" is tested") {
+		
+			auto match=r.Match("@rleahy.ca");
+			
+			THEN("It is not found to be a valid e-mail") {
+			
+				CHECK(!match);
+			
+			}
+		
+		}
+		
+		WHEN("\"John..Doe@example.com\" is tested") {
+		
+			auto match=r.Match("John..Doe@example.com");
+			
+			THEN("It is not found to be a valid e-mail") {
+			
+				CHECK(!match);
+			
+			}
+		
+		}
+		
+		WHEN("\"John.Doe@example.com\" is tested") {
+		
+			auto match=r.Match("John.Doe@example.com");
+			
+			THEN("It is found to be a valid e-mail") {
+			
+				CHECK(match);
+			
+			}
+		
+		}
+		
+		WHEN("\".John.Doe@example.com\" is tested") {
+		
+			auto match=r.Match(".John.Doe@example.com");
+			
+			THEN("It is not found to be a valid e-mail") {
+			
+				CHECK(!match);
+			
+			}
+		
+		}
+		
+		WHEN("\"John.Doe.@example.com\" is tested") {
+		
+			auto match=r.Match("John.Doe.@example.com");
+			
+			THEN("It is not found to be a valid e-mail") {
+			
+				CHECK(!match);
+			
+			}
+		
+		}
+	
+	}
+
+}
